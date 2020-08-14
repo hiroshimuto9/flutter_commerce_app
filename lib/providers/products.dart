fa68916677d6d30e028612f4c8ef77087347711a
mutohiroshi <mutohiroshi@team-lab.com>
2020-08-14 14:22:12 +0900

    add: display product title on detail screen by getting data from provider

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -39,4 +39,9 @@ class Products with ChangeNotifier {
   List<Product> get items {
     return [..._items];
   }
+
+  Product findById(String id) {
+    return _items.firstWhere((product) => product.id == id);
+  }
+
 }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_commerce_app/models/product.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetaiScreen extends StatelessWidget {
   static const routeName = '/product-detail';
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
     );
   }
 }
