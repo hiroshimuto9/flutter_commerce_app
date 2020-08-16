9bb525e3d599659e3805a5e7eb0ce8c5ba18e6ec
mutohiroshi <mutohiroshi@team-lab.com>
2020-08-16 14:58:22 +0900

    add: add addOrder function

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -23,4 +23,17 @@ class Orders with ChangeNotifier {
   List<OrderItem> get orders {
     return[..._orders];
   }
+
+  void addOrder(List<CartItem> cartProducts, double total) {
+    _orders.insert(
+      0,
+      OrderItem(
+        id: DateTime.now().toString(),
+        amount: total,
+        dateTime: DateTime.now(),
+        products: cartProducts,
+      ),
+    );
+    notifyListeners();
+  }
 }
