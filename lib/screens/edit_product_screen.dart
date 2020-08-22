ca057f824fc8b834c3abc16f7277cec1c3df396e
mutohiroshi <mutohiroshi@team-lab.com>
2020-08-23 00:04:32 +0900

    add: add lister

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -13,12 +13,25 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _imageUrlFocusNode = FocusNode();
 
   @override
+  void initState() {
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+
+  @override
   void dispose() {
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
     _imageUrlController.dispose();
     _imageUrlFocusNode.dispose();
     super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if(!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
   }
 
   @override
