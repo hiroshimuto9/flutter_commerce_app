0e3b888d2417ad3457219d2457b13d969a0a7dce
mutohiroshi <mutohiroshi@team-lab.com>
2020-08-23 15:23:01 +0900

    add: add addProduct method

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -45,6 +45,19 @@ class Products with ChangeNotifier {
   }
 
   Product findById(String id) {
-    return _items.firstWhere((product) => product.id == id);
+    return _items.
+    firstWhere((product) => product.id == id);
+  }
+
+  void addProduct(Product product) {
+    final newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
+    notifyListeners();
   }
 }
