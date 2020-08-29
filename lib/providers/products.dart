da059a623f6482b12052854df7f23ae6b0c85213
mutohiroshi <mutohiroshi@team-lab.com>
2020-08-29 22:56:05 +0900

    add: add post request to firebase

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'product.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 class Products with ChangeNotifier {
   List<Product> _items = [
     Product(
@@ -50,6 +53,15 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    const url = 'https://flutter-commerce-app.firebaseio.com/products.json';
+    http.post(url, body: json.encode({
+      'title': product.title,
+      'description': product.description,
+      'price': product.price,
+      'imageUrl': product.imageUrl,
+      'isFavorite': product.isFavorite
+    }),);
+
     final newProduct = Product(
       id: DateTime.now().toString(),
       title: product.title,
