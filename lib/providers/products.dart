743ee0321e24ddc45d60a6b074a2ab0b5214d57c
mutohiroshi <mutohiroshi@team-lab.com>
2020-09-05 19:11:24 +0900

    update: use async await and try-catch-finally

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -52,15 +52,19 @@ class Products with ChangeNotifier {
     firstWhere((product) => product.id == id);
   }
 
-  Future<void> addProduct(Product product) {
+  Future<void> addProduct(Product product) async {
     const url = 'https://flutter-commerce-app.firebaseio.com/products.json';
-    return http.post(url, body: json.encode({
-      'title': product.title,
-      'description': product.description,
-      'price': product.price,
-      'imageUrl': product.imageUrl,
-      'isFavorite': product.isFavorite
-    }),).then((response) {
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'title': product.title,
+          'description': product.description,
+          'price': product.price,
+          'imageUrl': product.imageUrl,
+          'isFavorite': product.isFavorite
+        }),
+      );
       final newProduct = Product(
         id: json.decode(response.body)['name'],
         title: product.title,
@@ -70,9 +74,9 @@ class Products with ChangeNotifier {
       );
       _items.add(newProduct);
       notifyListeners();
-    }).catchError((error) {
+    } catch (error) {
       throw error;
-    });
+    }
   }
 
   void updateProduct(String id, Product newProduct) {
--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -78,7 +78,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
     }
   }
 
-  void _saveForm() {
+  Future<void> _saveForm() async {
     final isValid = _form.currentState.validate();
     if (!isValid) {
       return;
@@ -94,30 +94,29 @@ class _EditProductScreenState extends State<EditProductScreen> {
       });
       Navigator.of(context).pop();
     } else {
-      Provider.of<Products>(context, listen: false)
-        .addProduct(_editedProduct)
-        .catchError((error) {
-          return showDialog<Null>(
-            context: context,
-            builder: (ctx) => AlertDialog(
-              title: Text('An error occuerred!!'),
-              content: Text('Something went wrong.'),
-              actions: <Widget>[
-                FlatButton(child: Text('Okay'), onPressed: () {
-                  Navigator.of(ctx).pop();
-                })
-              ],
-            ),
-          );
-        })
-        .then((_) {
-          setState(() {
-            _isLoading = false;
-          });
-          Navigator.of(context).pop();
-      });
+      try {
+        await Provider.of<Products>(context, listen: false)
+          .addProduct(_editedProduct);
+      } catch (error) {
+        await showDialog<Null>(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('An error occuerred!!'),
+            content: Text('Something went wrong.'),
+            actions: <Widget>[
+              FlatButton(child: Text('Okay'), onPressed: () {
+                Navigator.of(ctx).pop();
+              })
+            ],
+          ),
+        );
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+        Navigator.of(context).pop();
+      }
     }
-    // Navigator.of(context).pop();
   }
 
   @override
