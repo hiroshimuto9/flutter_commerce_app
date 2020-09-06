db0967dced1f4cf5b13ad98e153995d051bb498c
mutohiroshi <mutohiroshi@team-lab.com>
2020-09-06 15:42:19 +0900

    update: add error handling when push delete icon button

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -12,6 +12,7 @@ class UserProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
     return ListTile(
       title: Text(title),
       leading: CircleAvatar(
@@ -29,8 +30,16 @@ class UserProductItem extends StatelessWidget {
           ),
           IconButton(
             icon: Icon(Icons.delete),
-            onPressed: () {
-              Provider.of<Products>(context, listen: false).deleteProduct(id);
+            onPressed: () async {
+              try {
+                await Provider.of<Products>(context, listen: false).deleteProduct(id);
+              } catch (error) {
+                scaffold.showSnackBar(
+                  SnackBar(
+                    content: Text('deleting product failed', textAlign: TextAlign.center,),
+                  ),
+                );
+              }
             },
             color: Theme.of(context).errorColor,
           ),
