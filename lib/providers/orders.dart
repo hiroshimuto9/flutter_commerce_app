ffa97353dac612c8aa3d5db921339c98c68f05d2
mutohiroshi <mutohiroshi@team-lab.com>
2020-09-12 15:25:47 +0900

    add: create function to get orders from firebase

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -27,6 +27,37 @@ class Orders with ChangeNotifier {
     return[..._orders];
   }
 
+  Future<void> fetchandSetOrders() async {
+    final url = 'https://flutter-commerce-app.firebaseio.com/orders.json';
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if(extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          dateTime: DateTime.parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                      id: item['id'],
+                      price: item['price'],
+                      quantity: item['quantity'],
+                      title: item['title'],
+                    ),
+              )
+              .toList(),
+        ),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     final url = 'https://flutter-commerce-app.firebaseio.com/orders.json';
     final timestamp = DateTime.now();
@@ -38,7 +69,7 @@ class Orders with ChangeNotifier {
         'quantity': cp.quantity,
         'title': cp.title,
         'price': cp.price,
-      }).toString(),
+      }).toList(),
     }));
 
     _orders.insert(
