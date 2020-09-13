660592a8d5ca9dfc1fa9347a04c0c0fc1545e043
mutohiroshi <mutohiroshi@team-lab.com>
2020-09-13 14:26:24 +0900

    add: add login function

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,8 +7,10 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCmELgEmV4auMUPh78Vmlob5sL3HiHHIAo';
+  Future<void> _authenticate(
+    String email, String password, String urlSegment
+  ) async {
+    final url = 'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCmELgEmV4auMUPh78Vmlob5sL3HiHHIAo';
     final response = await http.post(
       url,
       body: json.encode(
@@ -19,5 +21,15 @@ class Auth with ChangeNotifier {
         }
       )
     );
+    // debug
+    print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
