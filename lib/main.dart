f25c90adc697dc35bf64544b92ca03463586db46
mutohiroshi <mutohiroshi@team-lab.com>
2020-09-19 22:44:51 +0900

    update: attaching orders to users

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,7 @@ class MyApp extends StatelessWidget {
           create:  null,
           update: (ctx, auth, previousOrder) => Orders(
             auth.token,
+            auth.userId,
             previousOrder == null ? [] : previousOrder.orders
           ),
         ),
